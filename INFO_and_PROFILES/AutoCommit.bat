93b6d0dd547dd10c3ff294e41a308db6f3d57efb
Aadi Sahni <73355073+AadiSahni@users.noreply.github.com>
2021-07-01 22:45:50 +0530

    AutoCommit YY-d_M:S

--- INFO_and_PROFILES/AutoCommit.bat
+++ INFO_and_PROFILES/AutoCommit.bat
@@ -1,6 +1,6 @@
 // this file itself doesn't commit anything, use task scheduler and import the xml file form github
 cd "C:\AHK\2nd-keyboard"
 git add --all
-git commit -m "AutoCommit %date+%YY %m %d %H:%M:%S"
+git commit -m "AutoCommit %date+%YY-%m-%d_%H:%M:%S"
 git push
 exit
